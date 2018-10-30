9379853db62927e59a45b869d9c9cbba33cd5bad
TheJuze <dimanikus0106@gmail.com>
2018-10-30 23:10:01 +0300

    ADD

---
+++ METODS/METODS/Program.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Threading;
+
+//Полином 17[П] + 5[Д]i+(1[А]+10[И]i)z+(3[В]+14[М]i)z^2+13[Л]z^3
+namespace PAVLOV_DMITRY_206
+{
+    struct Complex//структура для работы с комплексными числами
+    {
+        public double X, Y;
+
+        public Complex(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Complex operator -(Complex a)
+        {
+            a.X = -a.X;
+            a.Y = -a.Y;
+            return a;
+        }
+
+        public static Complex operator -(Complex a, Complex b)
+        {
+            a.X -= b.X;
+            a.Y -= b.Y;
+            return a;
+        }
+
+        public static Complex operator +(Complex a, Complex b)
+        {
+            a.X += b.X;
+            a.Y += b.Y;
+            return a;
+        }
+
+        public static Complex operator *(Complex a, Complex b)
+        {
+            return new Complex(a.X * b.X - a.Y * b.Y, a.X * b.Y + b.X * a.Y);
+        }
+
+        public static Complex operator *(double b, Complex a)
+        {
+            a.X *= b;
+            a.Y *= b;
+            return a;
+        }
+
+        public static Complex operator /(Complex a, Complex b)
+        {
+            a.X = (a.X * b.X - a.Y * b.Y) / (b.X * b.X + b.Y * b.Y);
+            a.Y = (a.Y * b.X + a.X * b.Y) / (b.X * b.X + b.Y * b.Y);
+            return a;
+        }
+
+        public static Complex operator /(double b, Complex a)
+        {
+            a.X /= b;
+            a.Y /= b;
+            return a;
+        }
+
+        public static Complex Power(Complex a, int power)
+        {
+            if (power == 0)
+            {
+                a.X = 1;
+                a.Y = 0;
+                return a;
+            }
+            Complex b = a;
+            for (int i = 2; i <= power; i++)
+            {
+                b = b * a;
+            }
+            return b;
+        }
+        public static Complex Value(Vector X)
+        {
+            return new Complex(X.X, X.Y);
+        }
+
+    }
+
+    struct Vector
+    {
+        public double X, Y;
+
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Vector operator -(Vector a, Vector b)
+        {
+            a.X -= b.X;
+            a.Y -= b.Y;
+            return a;
+        }
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            a.X += b.X;
+            a.Y += b.Y;
+            return a;
+        }
+
+        public static Vector operator *(double b, Vector a)
+        {
+            a.X *= b;
+            a.Y *= b;
+            return a;
+        }
+
+        public static Vector toVector(Complex X)
+        {
+            return new Vector(X.X,X.Y);
+        }
+
+        public static double Norm(Vector a)
+        {
+            return Math.Sqrt(a.X * a.X + a.Y * a.Y);
+        }
+
+    }
+
+    static class Polynom
+    {
+        //Коээфициенты полинома 17[П] + 5[Д]i+(1[А]+10[И]i)z+(3[В]+14[М]i)z^2+13[Л]z^3
+        static public Complex[] Coef = new Complex[4] { new Complex(17, 5), new Complex(1, 10), new Complex(3, 14), new Complex(13, 0) }; 
+
+       /* static public Complex Poly(Complex[] coef, Complex X)//вычисление полинома
+        {
+            Complex z = coef[0] + coef[1] * X + coef[2] * Complex.Power(X, 2) + coef[3] * Complex.Power(X, 3);
+            return z;
+        }*/
+
+        static public double getRe(Complex[] coef, Vector X)//вычисление действительной части полинома
+        {
+            return coef[0].X + coef[1].X * X.X - coef[1].Y * X.Y + coef[2].X * Math.Pow(X.X, 2) - 2 * coef[2].Y * X.X * X.Y -
+                   coef[2].X * Math.Pow(X.Y, 2) + coef[3].X * Math.Pow(X.X, 3) - 3 * coef[3].Y * Math.Pow(X.X, 2) * X.Y -
+                   3 * coef[3].X * X.X * Math.Pow(X.Y, 2) + coef[3].Y * Math.Pow(X.Y, 3);
+        }
+        static public double getRe_dx(Complex[] coef, Vector X)
+        {
+            return coef[1].X + 2 * coef[2].X * X.X - 2 * coef[2].Y * X.Y + 3 * coef[3].X * Math.Pow(X.X, 2) -
+                   6 * coef[3].Y * X.X * X.Y - 3 * coef[3].X * Math.Pow(X.Y, 2);
+        }
+        static public double getRe_dy(Complex[] coef, Vector X)
+        {
+            return -coef[1].Y - 2 * coef[2].Y * X.X - 2 * coef[2].X * X.Y - 3 * coef[3].Y * Math.Pow(X.X, 2) -
+                   6 * coef[3].X * X.X * X.Y + 3 * coef[3].Y * Math.Pow(X.Y, 2);
+        }
+
+        static public double getIm(Complex[] coef, Vector X)//вычисление мнимой части полинома
+        {
+            return coef[0].Y + X.X * coef[1].Y + coef[1].X * X.Y + coef[2].Y * Math.Pow(X.X, 2) + 2 * coef[2].X * X.X * X.Y -
+                   coef[2].Y * Math.Pow(X.Y, 2) + coef[3].Y * Math.Pow(X.X, 3) + 3 * coef[3].X * Math.Pow(X.X, 2) * X.Y -
+                   3 * coef[3].Y * X.X * Math.Pow(X.Y, 2) - coef[3].X * Math.Pow(X.Y, 3);
+        }
+        static public double getIm_dx(Complex[] coef, Vector X)//вычисление производной от мнимой части полинома по х
+        {
+            return coef[1].Y + 2 * coef[2].Y * X.X + 2 * coef[2].X * X.Y + 3 * coef[3].Y * Math.Pow(X.X, 2) +
+                   6 * coef[3].X * X.X * X.Y - 3 * coef[3].Y * Math.Pow(X.Y, 2);
+        }
+        static public double getIm_dy(Complex[] coef, Vector X)//вычисление производной от мнимой части полинома по у
+        {
+            return coef[1].X + 2 * coef[2].X * X.X - 2 * coef[2].Y * X.Y + 3 * coef[3].X * Math.Pow(X.X, 2) -
+                   6 * coef[3].Y * X.X * X.Y - 3 * coef[3].X * Math.Pow(X.Y, 2);
+        }
+
+        static public double Funct(Complex[] coef, Vector X)//вычисление функции |Polynom|^2
+        {
+            return Math.Pow(getRe(coef, X), 2) + Math.Pow(getIm(coef, X), 2);
+        }
+
+        static public Vector Grad(Complex[] coef, Vector X)//вычисление градиента от функции
+        {
+            return new Vector(2 * getRe(coef, X) * getRe_dx(coef, X) + 2 * getIm(coef, X) * getIm_dx(coef, X),
+                2 * getRe(coef, X) * getRe_dy(coef, X) + 2 * getIm(coef, X) * getIm_dy(coef, X));
+        }
+
+        static public Complex[] Horner_sMethod(Complex[] oldCoef, int power, Complex root) //Метод Горнера
+        {
+            Complex[] newCoef = new Complex[4];
+            if (power == 3)
+            {
+                newCoef[3] = new Complex(0, 0);
+                newCoef[2] = oldCoef[3];
+                newCoef[1] = oldCoef[2] + root * newCoef[2];
+                newCoef[0] = oldCoef[1] + root * newCoef[1];
+            }
+            else if (power == 2)
+            {
+                newCoef[3] = new Complex(0, 0);
+                newCoef[2] = new Complex(0, 0);
+                newCoef[1] = oldCoef[2];
+                newCoef[0] = oldCoef[1] + root * newCoef[1];
+            }
+            return newCoef;
+        }
+    }
+    class Program
+    {
+        public const double Eps = 1e-6;
+        static Complex[] coef = new Complex[4];
+        static Complex[] roots = new Complex[3];
+        static void CrushingGradient()//градиентный метод с дроблением шага
+        {
+            double a = 1, lambda = 0.65, delta = 0.5;
+            coef = Polynom.Coef;
+            Vector G= new Vector();
+            Vector X_k = new Vector();
+            int count = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                Vector X_i = new Vector(0, 0);//начальная точка
+                Console.WriteLine("x={0}, y={1}, f={2}", X_i.X, X_i.Y, Polynom.Funct(coef, Vector.toVector(Complex.Value(X_i))));
+                G = Polynom.Grad(coef, X_i);
+                while (Vector.Norm(G) > Eps)
+                {
+                    X_k = X_i - a * G;
+                   
+                  //  count++;
+                    if (Polynom.Funct(coef, X_k) - Polynom.Funct(coef, X_i) <= -a * delta * (Math.Pow(G.X, 2) + Math.Pow(G.Y, 2)))
+                    {
+
+                        X_i = X_k;
+                        Console.WriteLine("x={0}, y={1}, f={2:E3}", X_i.X, X_i.Y, Polynom.Funct(coef, Vector.toVector(Complex.Value(X_i))));
+                        G = Polynom.Grad(coef, X_k);
+                    }
+                    else a = a * lambda;
+                }
+                roots[i] = Complex.Value(X_k);
+                Console.WriteLine("_______________________________________________");
+                Console.WriteLine("{3} КОРЕНЬ: {0:00.00000000}+i*{1:00.00000000}={2:00.00000000}", roots[i].X, roots[i].Y, Polynom.Funct(coef, Vector.toVector(roots[i])),i+1);
+                Console.WriteLine("_______________________________________________");
+                coef = Polynom.Horner_sMethod(coef, 3 - i, roots[i]);
+            }
+            roots[2] = -coef[0] / coef[1];
+            Console.WriteLine("_______________________________________________");
+            Console.WriteLine("{3} КОРЕНЬ: {0:00.00000000}+i*{1:00.00000000}={2:00.00000000}", roots[2].X, roots[2].Y, Polynom.Funct(coef, Vector.toVector(roots[2])),3);
+            Console.WriteLine("_______________________________________________");
+        }
+        static void Main()
+        {
+            CrushingGradient();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ М.О.2/М.О.2/Program.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using static System.Math;
+//многомерная минимизация
+//методы нахождения минимума функции
+//Павлов Дмитрий 206 группа
+// A0 = Д + П*i = 5 + 17i 
+// А1 = И + А*i = 10 + i 
+// А2 = М + В*i = 14 + 3i 
+// А3 = Л*i = 13i 
+// f(x, y) = |А0+A1*z+A2*z^2+A3*z^3|^2 
+// функция имеет три локальных минимума
+//в x=-0.969085 y=0.245384
+//x=0.435595 y=-0.923594
+//x=0.436993 y=1.03343
+namespace М.О._2
+{
+    class Program
+    {
+        const double Eps = 1e-6;
+        const double Left = 0;
+        const double Right = 1.5;
+        
+        const double X1= 0,X2=0;//то4ка, с которой начинается минимизация
+        static double Funct(double x,double y)//возвращает значение функции в точке
+        {
+            return Pow(-39*x*x*y+14*x*x-6*x*y+10*x+13*y*y*y-14*y*y-y+5,2)+Pow(13*x*x*x+3*x*x-39*x*y*y+28*x*y+x-3*y*y-10*y+17,2);
+        }
+        static double Derivative_x(double x,double y)//первая производная по х
+        {
+        return 2*(-39*x*x*y+14*x*x-6*x*y+10*x+13*y*y*y-14*y*y-y+5)*(-78*x*y+28*x-6*y+10)+2*(13*x*x*x+3*x*x-39*x*y*y+28*x*y+x-3*y*y-10*y+17)*(26*x*x+6*x-39*y*y+28*y+1);
+        }
+        static double Derivative_y(double x, double y)//первая производная по у
+        {
+            return 2 * (-39 * x * x * y + 14 * x * x - 6 * x * y + 10 * x + 13 * y * y * y - 14 * y * y - y + 5) * (-39 * x * x - 6 * x + 39 * y * y - 28 * y - 1) + 2 * (13 * x * x * x + 3 * x * x - 39 * x * y * y + 28 * x * y + x - 3 * y * y - 10 * y + 17) * (-78 * x * y + 28 * x - 6 * y - 10);
+        }
+        static double F(double x, double y, double o)//слегка видоизмененная функция для МНГС // о - альфа
+        {
+            double q1 = Derivative_x(x, y);
+            double q2 = Derivative_y(x, y);
+            return Pow(-39 *(x-o*q1) * (x - o * q1) * (y - o * q2) + 14 * (x - o * q1) * (x - o * q1) - 6 * (x - o * q1) * (y - o * q2) + 10 * (x - o * q1) + 13 * (y - o * q2) * (y - o * q2) * (y - o * q2) - 14 * (y - o * q2) * (y - o * q2) - (y - o * q2) + 5, 2) + Pow(13 * (x - o * q1) * (x - o * q1) * (x - o * q1) + 3 * (x - o * q1) * (x - o * q1) - 39 * (x - o * q1) * (y - o * q2) * (y - o * q2) + 28 * (x - o * q1) * (y - o * q2) + (x - o * q1) - 3 * (y - o * q2) * (y - o * q2) - 10 * (y-o*q2) + 17, 2);
+        }
+        static double GR(double x,double y)//золотое сечение для МНГС
+        {
+            double a = Left;
+            double b = Right;
+            double s5 = Sqrt(5);
+            double c = (3 - s5) / 2 * (b - a) + a;
+            double d = (s5 - 1) / 2 * (b - a) + a;
+                while ((b - a) / 2 > Eps)
+                {
+                    if (F(x,y,c)<=F(x,y,d))
+                    {
+                        b = d;
+                        d = c;
+                        c = (3 - s5) / 2 * (b - a) + a;
+                    }
+                    else
+                    {
+                        a = c;
+                        c = d;
+                        d = (s5 - 1) / 2 * (b - a) + a;
+                    }
+                }
+            return (a + b) / 2;
+        }
+        static double GoldenRatio(int p, double k)//золотое сечение p=0 - x фиксирован, p=1 - y фиксирован, k - фиксированная переменная
+        {
+            double a = Left;
+            double b = Right;
+            double s5 = Sqrt(5);
+            double c = (3 - s5) / 2 * (b - a) + a;
+            double d = (s5 - 1) / 2 * (b - a) + a;
+            if (p==1)
+            {
+                while ((b - a) / 2 > Eps)
+                {
+                    if (Funct(c,k) <= Funct(d,k))
+                    {
+                        b = d;
+                        d = c;
+                        c = (3 - s5) / 2 * (b - a) + a;
+                    }
+                    else
+                    {
+                        a = c;
+                        c = d;
+                        d = (s5 - 1) / 2 * (b - a) + a;
+                    }
+                }
+            }
+            else
+            {
+                while ((b - a) / 2 > Eps)
+                {
+                    if (Funct(k,c) <= Funct(k,d))
+                    {
+                        b = d;
+                        d = c;
+                        c = (3 - s5) / 2 * (b - a) + a;
+                    }
+                    else
+                    {
+                        a = c;
+                        c = d;
+                        d = (s5 - 1) / 2 * (b - a) + a;
+                    }
+                }
+            }
+            return (a + b) / 2;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Функция |А0+A1*z+A2*z^2+A3*z^3|^2");
+            Console.WriteLine("A0 = Д + П*i = 5 + 17i");
+            Console.WriteLine("А1 = И + А*i = 10 + i");
+            Console.WriteLine("А2 = М + В*i = 14 + 3i");
+            Console.WriteLine("А3 = Л*i = 13i");
+            Console.WriteLine("Нажмите цифру для одномерной минимизации:");
+            Console.WriteLine("1.Метод покоординатного спуска");
+            Console.WriteLine("2.Метод градиентного спуска с дроблением шага");
+            Console.WriteLine("3.Метод градиентного спуска с постоянным шагом");
+            Console.WriteLine("4.Метод наискорейшего градиентного спуска");
+            Console.WriteLine("5.Метод градиентного спуска с заранее заданным шагом");
+            int key = Convert.ToInt16(Console.ReadLine());
+            switch (key)
+            {
+                case 1:
+                    CoordinateDescent();
+                    break;
+                case 2:
+                    CrushingGradient();
+                    break;
+                case 3:
+                    ConstantGradient();
+                    break;
+                case 4:
+                    FastestGradient();
+                    break;
+                case 5:
+                    DivergentSeries();
+                    break;
+            }
+        }
+        
+        static void CoordinateDescent()//метод покоординатного спуска
+        {
+            Console.WriteLine("метод покоординатного спуска");
+            double a = X1, b = X2;
+            double previous = Funct(a, b);
+            Console.WriteLine("x={0},y={1},F(x,y)={2}", a, b, previous);
+            a = GoldenRatio(1, b);
+            b = GoldenRatio(0, a);
+            double current = Funct(a, b);
+            while(Abs(current-previous)>Eps)
+            {
+                previous = current;
+                Console.WriteLine("x={0},y={1},F(x,y)={2}", a, b, previous);
+                a = GoldenRatio(1, b);
+                b = GoldenRatio(0, a);
+                current = Funct(a, b);
+            }
+            Console.WriteLine("Минимум достигается в точке:");
+            Console.WriteLine("x={0},y={1},F(x,y)={2}", a, b, current);
+            Console.ReadKey();
+        }
+
+        static void CrushingGradient()//метод градиентного спуска с дроблением шага
+        {
+            Console.WriteLine("метод градиентного спуска с дроблением шага");
+            double x = X1;
+            double y = X2;
+            double a=1;
+            double delta = 0.95;
+            Console.WriteLine("x={0},y={1},F(x,y)={2}", x, y, Funct(x,y));
+            //градиент (q1,q2) в 1 точке
+            double q1 = Derivative_x(x, y);
+            double q2 = Derivative_y(x, y);
+            //
+            double norm = Sqrt(q1 * q1 + q2 * q2);
+            while (norm >= Eps)
+            {
+                while (Funct(x-a*q1,y-a*q2)-Funct(x,y)>-a*Eps* (q1 * q1 + q2 * q2))
+                {
+                    a = a * delta;
+                }
+                x = x - a * q1;
+                y = y - a * q2;
+                Console.WriteLine("x={0},y={1},F(x,y)={2}", x, y, Funct(x, y));
+                q1 = Derivative_x(x, y);
+                q2 = Derivative_y(x, y);
+                norm = Sqrt(q1 * q1 + q2 * q2);
+            }
+            Console.WriteLine("Минимум достигается в точке:");
+            Console.WriteLine("x={0},y={1},F(x,y)={2}", x, y, Funct(x, y));
+            Console.ReadKey();
+        }
+        
+        static void ConstantGradient()//метод градиентного спуска с постоянным шагом
+        {
+            Console.WriteLine("метод градиентного спуска с постоянным шагом");
+            double a = 0.0001;//shag
+            double x = X1;
+            double y = X2;
+            Console.WriteLine("x={0},y={1},F(x,y)={2}", x, y, Funct(x, y));
+            //градиент (q1,q2)
+            double q1 = Derivative_x(x, y);
+            double q2 = Derivative_y(x, y);
+            //
+            double norm = Sqrt(q1 * q1 + q2 * q2);
+            while (norm>=Eps)
+            {
+                x = x - a * q1;
+                y = y - a * q2;
+                Console.WriteLine("x={0},y={1},F(x,y)={2}", x, y, Funct(x, y));
+                q1 = Derivative_x(x, y);
+                q2 = Derivative_y(x, y);
+                norm = Sqrt(q1 * q1 + q2 * q2);
+            }
+
+            Console.WriteLine("Минимум достигается в точке:");
+            Console.WriteLine("x={0},y={1},F(x,y)={2}", x, y, Funct(x, y));
+            Console.ReadKey();
+        }
+
+        static void DivergentSeries()//метод градиентного спуска с заранее заданным шагом
+        {
+            Console.WriteLine("метод градиентного спуска с заранее заданным шагом");
+            double x = X1;
+            double y = X2;
+            Console.WriteLine("x={0},y={1},F(x,y)={2}", x, y, Funct(x, y));
+            double k = 1000;
+            //градиент (q1,q2)
+            double q1 = Derivative_x(x, y);
+            double q2 = Derivative_y(x, y);
+            //
+            double norm = Sqrt(q1 * q1 + q2 * q2);
+            while (norm >= Eps)
+            {
+                k++;
+                x = x - q1/k;
+                y = y - q2/k;
+                Console.WriteLine("x={0},y={1},F(x,y)={2}", x, y, Funct(x, y));
+                q1 = Derivative_x(x, y);
+                q2 = Derivative_y(x, y);
+                norm = Sqrt(q1 * q1 + q2 * q2);
+            }
+            Console.WriteLine("Минимум достигается в точке:");
+            Console.WriteLine("x={0},y={1},F(x,y)={2}", x, y, Funct(x, y));
+            Console.ReadKey();
+        }
+
+        static void FastestGradient()//метод наискорейшего градиентного спуска
+        {
+            Console.WriteLine("метод наискорейшего градиентного спуска");
+            double x = X1;
+            double y = X2;
+            double a;
+            Console.WriteLine("x={0},y={1},F(x,y)={2}", x, y, Funct(x, y));
+            //градиент (q1,q2)
+            double q1 = Derivative_x(x, y);
+            double q2 = Derivative_y(x, y);
+            //
+            double norm = Sqrt(q1 * q1 + q2 * q2);
+            while (norm >= Eps)
+            {
+                a=GR(x, y);
+                x = x - a*q1;
+                y = y - a*q2;
+                Console.WriteLine("x={0},y={1},F(x,y)={2}", x, y, Funct(x, y));
+                q1 = Derivative_x(x, y);
+                q2 = Derivative_y(x, y);
+                norm = Sqrt(q1 * q1 + q2 * q2);
+            }
+            Console.WriteLine("Минимум достигается в точке:");
+            Console.WriteLine("x={0},y={1},F(x,y)={2}", x, y, Funct(x, y));
+            Console.ReadKey();
+        }
+    }
+}
